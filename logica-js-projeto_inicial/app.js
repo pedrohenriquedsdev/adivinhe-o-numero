87b2d9d14c863e031a5090d43a9f71d16ad764cb
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2025-10-06 10:14:48 -0300

    Fator tentativas adicionado

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+alert('Boas Vindas ao Jogo do Número Secreto!');
+
+let numeroSecreto = 5;
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = Number(prompt('Escolha um número entre 1 e 10'));
+
+    if (chute === numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}.`);
+        } else {
+            alert(`O número secreto é maior que ${chute}.`);
+        }
+    }
+    tentativas++;
+}
+
+alert(`Correto! Você acertou o número secreto com ${tentativas} tentativas. Aqui está -> ${numeroSecreto}`);
+
